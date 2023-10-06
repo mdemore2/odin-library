b5cadb08d729baaa0170d3b27b8db50faa6e28b1
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-10-06 10:57:33 -0400

    Added book modal, remove book and mark read button
    Dialog added to index.html
    javascript event listeners and neccessary funcs added in javascript.js

--- javascript.js
+++ javascript.js
@@ -8,18 +8,68 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.getElementById('title');
+  let author = document.getElementById('author');
+  let pages = document.getElementById('pages');
+  let read = document.getElementById('read');
+
+  let newBook = new Book(title.value, author.value, parseInt(pages.value), read.checked);
+  myLibrary.push(newBook);
+  displayBook(newBook);
 }
 
 function displayBook(book) {
-    container = document.getElementById('container');
-    card = document.createElement('div');
+    let container = document.getElementById('container');
+    let card = document.createElement('div');
     card.className = "card";
-    card.innerHTML = `<ul><li>${book.title}</li><li>${book.author}</li><li>${book.pages}</li><li>${book.read}</li></ul>`;
+    card.innerHTML = `<ul><li class='bookTitle'>${book.title}</li><li>${book.author}</li><li>${book.pages}</li><li class='readStatus'>${book.read}</li></ul>`;
+    card.innerHTML += `<button class='remove'>Remove Book</button><button class='markRead'>Mark as Read</button>`;
     container.appendChild(card);
+    addCardListener(card);
 }
 
 myLibrary.forEach((element) => {
   console.log(element);
   displayBook(element);
 });
+
+
+const dialog = document.querySelector('dialog');
+const openButton = document.getElementById('open');
+const closeButton = document.getElementById('close');
+
+openButton.addEventListener("click", () => {
+  dialog.showModal();
+})
+
+closeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  dialog.close();
+  console.log(myLibrary);
+})
+
+
+function addCardListener(card){
+    let removeButton = card.querySelector('.remove');
+    let readButton = card.querySelector('.markRead');
+
+    removeButton.addEventListener("click", (event) => {
+      let card = event.target.parentElement;
+      let title = card.querySelector('.bookTitle');
+      //myLibrary = myLibrary.filter(function(el) { return el.title != title.value} );
+      myLibrary.splice(myLibrary.findIndex(el => el.title === title.innerText), 1);
+      card.remove();
+    })
+
+    readButton.addEventListener("click", (event) => {
+      let card = event.target.parentElement;
+      let title = card.querySelector('.bookTitle');
+      let read = card.querySelector('.readStatus');
+      let i = myLibrary.findIndex(el => el.title === title.innerText);
+      myLibrary[i].read = !myLibrary[i].read;
+      read.innerText = myLibrary[i].read;
+    })
+
+
+}
